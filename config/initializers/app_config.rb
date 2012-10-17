38c52b94cb213e23f441789fcbd118e8b1502f19
Anurag Mohanty <anurag@columbia.edu>
2012-10-17 14:21:03 -0400

    slight update to app_config

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,2 +1,4 @@
-APP_CONFIG = YAML.load_file("#{Rails.root}/config/app_config.yml")[Rails.env]
+if File.exists?("#{Rails.root}/config/app_config.yml")
+	APP_CONFIG = YAML.load_file("#{Rails.root}/config/app_config.yml")[Rails.env]
+end
 

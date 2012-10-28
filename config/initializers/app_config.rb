db9560e246d9172acf43c7c4eea3eda821405cec
Anurag Mohanty <anurag@columbia.edu>
2012-10-28 01:32:10 -0400

    added google analytics

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,4 +1,5 @@
 if File.exists?("#{Rails.root}/config/app_config.yml")
 	APP_CONFIG = YAML.load_file("#{Rails.root}/config/app_config.yml")[Rails.env]
+	GA.tracker = APP_CONFIG['GOOGLE_TOKEN'] if !APP_CONFIG['GOOGLE_TOKEN'].blank?
 end
 

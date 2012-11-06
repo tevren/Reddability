f1fe7ade8c113825b229dc69e068d66dafb7a4c2
Anurag Mohanty <anurag@columbia.edu>
2012-11-06 11:38:07 -0500

    added customized titles that display subreddit and article information

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,13 @@
 module ApplicationHelper
 	def app_revision
-    revision_file = File.join(Rails.root, "VERSION")
-    if File.readable?(revision_file)
-      IO.read(revision_file)
-    else
-      "unknown"
-    end
-  end
+    	revision_file = File.join(Rails.root, "VERSION")
+    	if File.readable?(revision_file)
+      		IO.read(revision_file)
+    	else
+      		"unknown"
+    	end
+  	end
+	def title(page_title)
+  		content_for(:title) { page_title }
+	end
 end

2432f97411a96fb2cdc1062f3d44189dbf93559d
Anurag Mohanty <tevren@gmail.com>
2012-11-09 19:12:57 -0500

    cleaned up reddits controller, and added method to retrieve submission based on id

--- app/controllers/reddits_controller.rb
+++ app/controllers/reddits_controller.rb
@@ -1,3 +1,4 @@
+require 'digest/sha1'
 class RedditsController < ApplicationController
   caches_action :show, :expires_in => 3.minutes
   caches_action :subreddit, :expires_in => 1.minutes
@@ -25,16 +26,21 @@ class RedditsController < ApplicationController
 
   def show
     @original_url = Base64.urlsafe_decode64(params[:id]).to_s
-    @submission = params[:submission]
+    article = get_reddit_article(params[:article], params[:subreddit])
+    if article.class.to_s.match(/array/i)
+      @rarticle = article.first
+    else
+      @rarticle = article
+    end
     @subreddit = params[:subreddit].to_s
-    @submission_id = params[:submission][:id].gsub("t3_","")
-    @submission_title = params[:submission][:title].downcase.gsub(" ","_")
-    @reddit_url = "http://reddit.com/r/" + @subreddit + "/comments/" + @submission_id + "/" + @submission_title
+    @submission_title = @rarticle.title
+    @reddit_url = "http://reddit.com" + @rarticle.permalink
+    @ckey = Digest::SHA1.hexdigest(params[:id])
     biffbot = Biffbot::Base.new(APP_CONFIG['DIFFBOT_TOKEN'])
     options = Hash.new
     options[:tags] = true
     options[:stats] = true
-    options[:submission_title] = params[:submission][:title]
+    options[:submission_title] = @submission_title
     @article = get_content_from_url(@original_url,options)
     logger.info "#{@article[:stats].to_s}" 
     @subs = subs
@@ -47,6 +53,7 @@ class RedditsController < ApplicationController
   def subreddit
     r = Reddit::Api.new
   	@articles = r.browse("#{params[:id]}", {:limit => 100, :section => "hot",:cookie => session[:cookie]})
+    @subreddit = params[:id]
     @subs = subs
     respond_to do |format|
       format.html
@@ -58,12 +65,19 @@ class RedditsController < ApplicationController
     unless url.match(/(jpg|png|gif|jpeg|bmp|tiff)$/)
       biffbot = Biffbot::Base.new(APP_CONFIG['DIFFBOT_TOKEN'])
       article = biffbot.parse(@original_url,options)
-    else
+    else      
       article = Hash.new
       article[:text] = "<img src='#{url}'>"
       article[:title] = options[:submission_title] if options[:submission_title]
     end
     return article
+  end
+
+  def get_reddit_article(id,subreddit)
+    r = Reddit::Api.new
+    r.browse(subreddit, {:limit => 100, :section => "hot",:cookie => session[:cookie]}).each do |story|
+      return story if story.id == id
+    end
   end
 
 end
